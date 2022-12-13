3677035aa970b922a090955cadbcd8b7153b10f2
Johannes Niedens <johannesniedens@gmail.com>
2022-12-13 16:12:32 +0100

    libft libft.h updated

--- 0.0__libft/libft.h
+++ 0.0__libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 23:47:27 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/13 01:31:26 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/13 16:03:34 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,17 @@
 #include <unistd.h>
 #include <ctype.h>
 
-int ft_isupper(int c);
-int ft_islower(int c);
+int ft_isalnum(int c);
 int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_iscntrl(int c);
+int ft_isdigit(int c);
+int ft_isgraph(int c);
+int ft_islower(int c);
+int ft_isprint(int c);
+int ft_ispunct(int c);
+int ft_isspace(int c);
+int ft_isupper(int c);
+int ft_isxdigit(int c):
 
 #endif
