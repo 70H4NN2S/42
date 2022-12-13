c0434ea0f92dac6286046541c5aee5c072a87d4a
Johannes Niedens <johannesniedens@gmail.com>
2022-12-13 15:56:30 +0100

    gitignore added with java, c, os and text editor coverage

--- 0.0__libft/ft_islower.c
+++ 0.0__libft/ft_islower.c
@@ -6,7 +6,7 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 00:10:39 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/13 00:16:55 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/13 14:33:39 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,3 @@ int ft_islower(int c)
 {
   return (c >= 'a' && c <= 'z');
 }
-/*
-also some strange behavior with 
-int main(void)
-{
-  printf("%d\n", ft_islower('b'));
-  printf("%d", islower('b'));
-}
-*/
--- 0.0__libft/ft_isupper.c
+++ 0.0__libft/ft_isupper.c
@@ -6,7 +6,7 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 23:53:11 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/13 00:17:10 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/13 14:33:49 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,3 @@ int ft_isupper(int c)
 {
   return (c >= 'A' && c <= 'Z');
 }
-/*
-Strange behavior with the not zero returned value!!!
-
-
-int main(void)
-{
-  printf("%d\n", ft_isupper('Z'));
-  printf("%d", isupper(65));
-  printf("%d\n", ft_isupper('A'));
-  printf("%d", isupper(90));
-}
-*/
