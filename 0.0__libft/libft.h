320c28d4e28aa4fc8938fade47d0cc4f55c76a67
Johannes Niedens <johannesniedens@gmail.com>
2022-12-13 01:32:26 +0100

    header file created

---
+++ 0.0__libft/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 23:47:27 by jniedens          #+#    #+#             */
+/*   Updated: 2022/12/13 01:31:26 by jniedens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+#include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+
+int ft_isupper(int c);
+int ft_islower(int c);
+int ft_isalpha(int c);
+
+#endif
