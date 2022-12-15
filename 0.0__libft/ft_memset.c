9ad60b591f82b7cef0efef58f6113e985e788ac9
Johannes Niedens <johannesniedens@gmail.com>
2022-12-15 16:37:02 +0100

    libft memset

--- 0.0__libft/ft_memset.c
+++ 0.0__libft/ft_memset.c
@@ -1,33 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/12 23:47:27 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/15 16:36:35 by jniedens         ###   ########.fr       */
+/*   Created: 2022/12/15 16:09:59 by jniedens          #+#    #+#             */
+/*   Updated: 2022/12/15 16:36:10 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdio.h>
-# include <unistd.h>
-# include <ctype.h>
-# include <string.h>
-# include <stdlib.h>
+#include "libft.h"
 
-int ft_atoi(const char *s);
+void  *ft_memset(void *s, int c, size_t n)
+{
+  unsigned char *ptr;
 
-int ft_isalnum(int c);
-int ft_isalpha(int c);
-int ft_isascii(int c);
-int ft_isdigit(int c);
-int ft_isprint(int c);
-
-size_t  ft_strlen(const char *s);
-
-void  *ft_memset(void *s, int c, size_t n);
-
-#endif
+  ptr = (unsigned char*) s;
+  while (n-- > 0)
+    *(ptr++) = (unsigned char) c;
+  return (s);
+}
+/*
+int main () {
+  char text[] = "abcdefghij";
+  char *res = memset(text, '-', 5);
+  char *ft_res = ft_memset(text, '-', 5);
+  printf("memset   : %s\n", res);
+  printf("ft_memset: %s\n", ft_res);
+}
+*/
