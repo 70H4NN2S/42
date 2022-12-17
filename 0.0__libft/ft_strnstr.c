6b6d3fc6c6f5acf291b6869d9cd489478708a291
Your Name <johannesniedens@gmail.com>
2022-12-17 17:04:52 +0100

    libft strnstr

--- 0.0__libft/ft_strnstr.c
+++ 0.0__libft/ft_strnstr.c
@@ -1,31 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jniedens <jniedens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/17 15:47:31 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/17 17:02:57 by jniedens         ###   ########.fr       */
+/*   Created: 2022/12/17 16:41:56 by jniedens          #+#    #+#             */
+/*   Updated: 2022/12/17 17:03:09 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	uc;
+	size_t	i;
 
+	if (n == 0)
+		return (NULL);
 	i = 0;
-	uc = (unsigned char) c;
-	while (s[i])
-	{
-		if (s[i] == uc)
-			return ((char *)&s[i]);
-		i++;
-	}
-	if (!uc)
-		return ((char *)&s[i]);
-	return (NULL);
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
+		++i;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- 0.0__libft/libft.h
+++ 0.0__libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jniedens <jniedens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 23:47:27 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/17 15:51:39 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/17 16:46:17 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
