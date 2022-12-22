97926b2539874628d0f7021c97c21a76f4bf571a
Johannes Niedens <johannesniedens@gmail.com>
2022-12-22 01:58:08 +0100

    libft substr

--- 0.0__libft/ft_substr.c
+++ 0.0__libft/ft_substr.c
@@ -12,14 +12,14 @@
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*newstr;
 
 	if (!s)
 		return (NULL);
 	if (ft_strlen(s) < start)
-		len = 0;
+		return (ft_strdup(""));
 	if (ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
 	newstr = (char *) malloc(sizeof(char) * (len + 1));
