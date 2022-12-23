acbae1a87c4e4576c13b6c83e629408dccd940dc
Johannes Niedens <johannesniedens@gmail.com>
2022-12-23 13:57:46 +0100

    libft refactor output functions

--- 0.0__libft/ft_putnbr_fd.c
+++ 0.0__libft/ft_putnbr_fd.c
@@ -3,52 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jniedens <johannesniedens@gmail.com>       +#+  +:+       +#+        */
+/*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 00:36:59 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/21 13:37:38 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/23 13:36:49 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_getlen(int n)
-{
-	if (n >= 10)
-		return (ft_getlen(n / 10) + 1);
-	else if (n <= -10)
-		return (ft_getlen(n / -10) + 2);
-	else if (n < 0)
-		return (2);
-	else
-		return (1);
-}
-
-static int	ft_getnum(long n, int unit)
-{
-	while (unit--)
-		n /= 10;
-	return (n % 10);
-}
-
 void	ft_putnbr_fd(int n, int fd)
 {
-	int		i;
-	int		len;
-	long	nbr;
-
-	i = 0;
-	nbr = n;
-	len = ft_getlen(n);
-	if (nbr < 0)
+	if (n < 0)
 	{
+		if (n == -2147483648)
+		{
+			ft_putstr_fd("-2147483648", fd);
+			return ;
+		}
 		ft_putchar_fd('-', fd);
-		i++;
-		nbr *= -1;
+		n = -n;
 	}
-	while (len - i)
+	if (n / 10)
 	{
-		ft_putchar_fd('0' + ft_getnum(nbr, len - i - 1), fd);
-		i++;
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
+	else
+		ft_putchar_fd(('0' + n), fd);
 }
--- 0.0__libft/libft.h
+++ 0.0__libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 23:47:27 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/23 12:32:34 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/23 13:38:43 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,12 @@ int		ft_isprint(int c);
 /* strings */
 char	*ft_itoa(int n);
 
+/* output */
 void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char	*s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strrchr(const char *s, int c);
@@ -53,7 +56,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-void	ft_putendl_fd(char	*s, int fd);
 char	*ft_strdup(const char *s);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
