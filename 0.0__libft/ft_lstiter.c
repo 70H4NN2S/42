81617f94ee89b12bc6fbb80748104639e0155f08
Johannes Niedens <johannesniedens@gmail.com>
2022-12-25 13:00:16 +0100

    libft ft_lstiter

--- 0.0__libft/ft_lstiter.c
+++ 0.0__libft/ft_lstiter.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/25 11:48:44 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/25 12:58:44 by jniedens         ###   ########.fr       */
+/*   Created: 2022/12/25 12:51:05 by jniedens          #+#    #+#             */
+/*   Updated: 2022/12/25 12:56:11 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_lstiteri(t_list *lst, void (*del)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst)
 		return ;
 	if (lst -> next != NULL)
-		ft_lstiteri(lst -> next, (*del));
-	ft_lstdelone(lst, (*del));
-}
-
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	ft_lstiteri(*lst, (*del));
-	*lst = NULL;
+		ft_lstiter(lst -> next, (*f));
+	(*f)(lst -> content);
 }
--- 0.0__libft/libft.h
+++ 0.0__libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 23:47:27 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/25 12:37:28 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/25 12:55:17 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,5 +74,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
