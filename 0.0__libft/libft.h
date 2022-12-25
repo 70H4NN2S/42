85fbd28bcf21c85ad6045b139350e8d035baa2f0
Johannes Niedens <johannesniedens@gmail.com>
2022-12-25 11:19:28 +0100

    libft ft_lstsize

--- 0.0__libft/libft.h
+++ 0.0__libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 23:47:27 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/25 11:13:20 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/25 11:18:07 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,5 +69,6 @@ typedef struct s_list
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 #endif
