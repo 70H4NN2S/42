edb23a54a41d76ad7ac77708686ed2c933c95f5e
Johannes Niedens <johannesniedens@gmail.com>
2022-12-25 12:49:44 +0100

    libft ft_lstclear

--- 0.0__libft/libft.h
+++ 0.0__libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 23:47:27 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/25 11:47:39 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/25 12:37:28 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,5 +73,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
