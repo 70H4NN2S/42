c404e8a5f24d93cde3803810aad9447fd7b7b1cb
Your Name <johannesniedens@gmail.com>
2022-12-27 16:07:11 +0100

    get_next_line start

---
+++ 1.0__get-next-line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jniedens <jniedens@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 15:44:22 by jniedens          #+#    #+#             */
+/*   Updated: 2022/12/27 15:46:43 by jniedens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <sys/uio.h>
+# include <sys/types.h>
+
+char	*get_next_line(int fd);
+
+#endif
