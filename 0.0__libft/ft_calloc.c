25ed7bee8695d5721089771897e5c3d6da90de93
Your Name <johannesniedens@gmail.com>
2022-12-28 11:46:35 +0100

    libft calloc bug fix

--- 0.0__libft/ft_calloc.c
+++ 0.0__libft/ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: jniedens <jniedens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 22:06:28 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/23 11:00:47 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/28 11:43:39 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
+	if (nmemb && (nmemb * size) / nmemb != size)
+		return (NULL);
 	ptr = (void *) malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
--- 1.0__get-next-line/get_next_line.c
+++ 1.0__get-next-line/get_next_line.c
@@ -6,13 +6,71 @@
 /*   By: jniedens <jniedens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 15:25:24 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/27 16:04:26 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/28 10:13:30 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+#define BUFF_SIZE  32
 
-char	*get_next_line(int fd)
+int	get_next_line(int const fd, char **line)
 {
-	
+    char buffer[BUFF_SIZE + 1];
+    size_t i;
+    size_t size;
+
+    if (!(*line = malloc(BUFF_SIZE)))
+        return (-1);
+    *line[0] = '\0';
+    while (read(fd, buffer, BUFF_SIZE) > 0)
+    {
+        buffer[BUFF_SIZE + 1] = '\0';
+        if (buffer[0] == '\n')
+            return (1);
+        if (ft_strchr(buffer, '\n') != NULL)
+        {
+            i = 0;
+            size = ft_strlen(*line);
+            buffer[BUFF_SIZE + 1] = '\0';
+            while (buffer[i] != '\0')
+            {
+                if (buffer[i] == '\n')
+                {
+                    if (!(*line = realloc(*line, i + size)))
+                        return (-1);
+                    buffer[i] = '\0';
+                    ft_strncat(*line, buffer, i);
+                    return (1);
+                }
+                i++;
+            }
+        }
+        if (!(*line = realloc(*line, BUFF_SIZE)))
+            return (-1);
+        ft_strncat(*line, buffer, BUFF_SIZE);
+    }
+    return (0);
+}
+
+int     main(void)
+{
+    int     fd;
+    int     ret;
+    char    *line;
+
+    if ((fd = open("main.c", O_RDONLY)) < 3 && fd != 0)
+        return (-1);
+    printf("%d\n", fd);
+    ret = get_next_line(fd, &line);
+    printf("%d - %s\n", ret, line);
+    ret = get_next_line(fd, &line);
+    printf("%d - %s\n", ret, line);
+    ret = get_next_line(fd, &line);
+    printf("%d - %s\n", ret, line);
+    ret = get_next_line(fd, &line);
+    printf("%d - %s\n", ret, line);
+    return (0);
 }
