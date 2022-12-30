cef321f00e03eb030d87bdbfcf370f1817b96106
Johannes Niedens <johannesniedens@gmail.com>
2022-12-30 20:35:58 +0100

    ft_printf %s flag added

--- 1.1__ft-printf/ft_printf.c
+++ 1.1__ft-printf/ft_printf.c
@@ -6,35 +6,60 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 11:49:38 by jniedens          #+#    #+#             */
-/*   Updated: 2022/12/30 17:32:30 by jniedens         ###   ########.fr       */
+/*   Updated: 2022/12/30 20:35:20 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <unistd.h>
 
-void	ft_check(char c)
+void	ft_putchar_fd(char c, int fd)
 {
-	if (c == 'i')
-		ft_putchar_fd('i', 1);
+	write(fd, &c, 1);
+}
+
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(fd, s, len);
+}
+
+void	ft_check(char c, va_list args)
+{
+	char	*arg;
+
+	arg = va_arg(args, char *);
+	if (c == 's')
+		ft_putstr_fd(arg, 1);
 }
 
 int	ft_printf(const char *str, ...)
 {
+	va_list	args;
 	int		i;
 
+	if (!str)
+		return (0);
 	i = 0;
+	va_start(args, str);
 	while (str[i])
 	{
 		if (str[i] == '%')
-			ft_check(str[++i]);
+			ft_check(str[++i], args);
 		else
 			ft_putchar_fd(str[i], 1);
 		i++;
 	}
-	return (0);
+	va_end(args);
+	return (i);
 }
 
 int	main(void)
 {
-	ft_printf("Hello");
+	ft_printf("He%s, i am %s.", "llo", "Johannes", "asd");
 }
