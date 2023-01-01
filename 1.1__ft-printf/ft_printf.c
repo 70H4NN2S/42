9625e7edca327317258e8727f06a1fff9d373ac4
Johannes Niedens <johannesniedens@gmail.com>
2023-01-01 10:58:22 +0100

    testing everything in a single file

--- 1.1__ft-printf/ft_printf.c
+++ 1.1__ft-printf/ft_printf.c
@@ -6,19 +6,85 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 11:49:38 by jniedens          #+#    #+#             */
-/*   Updated: 2023/01/01 10:38:58 by jniedens         ###   ########.fr       */
+/*   Updated: 2023/01/01 10:57:55 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
 
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_putnbr(int n);
-void	ft_putunbr(unsigned int n);
-void	ft_puthexlow(unsigned int n);
-void	ft_puthexup(unsigned int n);
-void	ft_putvoidhex(unsigned long n);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = n * -1;
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	ft_putchar(n % 10 + '0');
+}
+
+void	ft_putunbr(unsigned int n)
+{
+	if (n >= 10)
+		ft_putunbr(n / 10);
+	ft_putchar(n % 10 + '0');
+}
+
+void	ft_puthexlow(unsigned int n)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	if (n >= 16)
+		ft_puthexlow(n / 16);
+	ft_putchar(hex[n % 16]);
+}
+
+void	ft_puthexup(unsigned int n)
+{
+	char	*hex;
+
+	hex = "0123456789ABCDEF";
+	if (n >= 16)
+		ft_puthexup(n / 16);
+	ft_putchar(hex[n % 16]);
+}
+
+void	ft_putvoidhex(unsigned long long n)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	if (n >= 16)
+		ft_putvoidhex(n / 16);
+	ft_putchar(hex[n % 16]);
+}
 
 void	ft_check(char c, va_list args)
 {
@@ -50,7 +116,10 @@ int	ft_printf(const char *str, ...)
 		if (str[i] == '%')
 		{
 			i++;
-			ft_check(str[i], args);
+			if (str[i] == '%')
+				ft_putchar('%');
+			else
+				ft_check(str[i], args);
 		}
 		else
 			ft_putchar(str[i]);
@@ -60,7 +129,22 @@ int	ft_printf(const char *str, ...)
 	return (0);
 }
 
-int	main(void)
+int		main(void)
 {
-	ft_printf("He%s, i am %s.", "llo", "Johannes", "asd");
+	//No Flag
+	printf("\nprintf no flag string test\n");
+	ft_printf("ft_printf no flag string test\n");
+
+	//%%
+	printf("printf\t\t: ");
+	printf("%%\n");
+	ft_printf("ft_printf\t: ");
+	ft_printf("%%\n");
+
+	//d
+	printf("printf\t\t: ");
+	printf("%d\n", 650);
+	ft_printf("ft_printf\t: ");
+	ft_printf("%d\n", 650);
+    return (0);
 }
