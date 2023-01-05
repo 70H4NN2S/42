2150f12e733ce16667cc707a9cf0239070944766
Johannes Niedens <johannesniedens@gmail.com>
2023-01-06 00:10:15 +0100

    get_next_line

--- 1.0__get-next-line/get_next_line_utils.c
+++ 1.0__get-next-line/get_next_line_utils.c
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/27 15:44:22 by jniedens          #+#    #+#             */
-/*   Updated: 2023/01/06 00:08:35 by jniedens         ###   ########.fr       */
+/*   Created: 2023/01/06 00:09:58 by jniedens          #+#    #+#             */
+/*   Updated: 2023/01/06 00:09:59 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# include <unistd.h>
-# include <sys/uio.h>
-# include <sys/types.h>
-# include <stdlib.h>
-
-char	*get_next_line(int fd);
-
-#endif
