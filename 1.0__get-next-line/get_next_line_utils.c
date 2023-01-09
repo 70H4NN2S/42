ce9a67adc07e9eac48b521e692d66cddb9dd1532
Johannes Niedens <johannesniedens@gmail.com>
2023-01-09 22:53:46 +0100

    get_next_line helper functions

--- 1.0__get-next-line/get_next_line_utils.c
+++ 1.0__get-next-line/get_next_line_utils.c
@@ -6,7 +6,68 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 00:09:58 by jniedens          #+#    #+#             */
-/*   Updated: 2023/01/06 00:09:59 by jniedens         ###   ########.fr       */
+/*   Updated: 2023/01/09 22:53:13 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+	{
+		if (!*s++)
+			return (NULL);
+	}
+	return ((char *)s);
+}
+
+char	*ft_strdupchar(const char *s1, char c)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	while (s1[i] != c)
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i] != c)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
