e375be31959f9452d4bde5f89522bec328087583
Johannes Niedens <johannesniedens@gmail.com>
2023-01-11 13:54:58 +0100

    gnl ft_read

--- 1.0__get-next-line/get_next_line.c
+++ 1.0__get-next-line/get_next_line.c
@@ -6,11 +6,47 @@
 /*   By: jniedens <jniedens@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 15:25:24 by jniedens          #+#    #+#             */
-/*   Updated: 2023/01/11 13:44:32 by jniedens         ###   ########.fr       */
+/*   Updated: 2023/01/11 13:54:17 by jniedens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/**
+ * @brief	read data from fd and append it to line string until newline is found
+ *
+ * @param	int fd			file descriptor
+ * @param	char **buffer	pointer to buffer string
+ * @return	int bytes_read	last bytes read from fd
+ */
+static int	ft_read(int fd, char **buffer)
+{
+	char	*temp;
+	char	*buf;
+	int		bytes_read;
+
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (-1);
+	bytes_read = read(fd, buf, BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		buf[bytes_read] = '\0';
+		if (!*buffer)
+			*buffer = ft_strdup(buf);
+		else
+		{
+			temp = ft_strjoin(*buffer, buf);
+			free(*buffer);
+			*buffer = temp;
+		}
+		if (ft_strchr(*buffer, '\n'))
+			break ;
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+	}
+	free(buf);
+	return (bytes_read);
+}
 
 /**
  * @brief	returns a line read from a file descriptor
